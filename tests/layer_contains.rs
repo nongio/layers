401f8b246895c45afb377ef847b66c2131c17934
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2024-09-29 08:54:49 +0200

    layer contains tests

--- tests/layer_contains.rs
+++ tests/layer_contains.rs
@@ -1,24 +1,26 @@
 use std::f64::consts::PI;
 
-// use layers::engine::node::ContainsPoint;
-use layers::engine::LayersEngine;
 use layers::types::{Point, Point3d};
+use layers::{engine::LayersEngine, types::Size};
 
 #[test]
 pub fn layer_contains() {
     let engine = LayersEngine::new(1000.0, 1000.0);
     let layer = engine.new_layer();
+    layer.set_size(Size::points(100.0, 100.0), None);
+
     engine.scene_add_layer(layer.clone());
 
-    // layer.set_size(Point { x: 100.0, y: 100.0 }, None);
+    engine.update(0.016);
 
-    // assert!(layer.bounds().contains(Point { x: 50.0, y: 50.0 }));
-    // assert!(!layer.bounds().contains(Point { x: 200.0, y: 50.0 }));
+    assert!(layer.cointains_point((50.0, 50.0)));
+    assert!(!layer.cointains_point((200.0, 50.0)));
 
-    // layer.set_position(Point { x: 100.0, y: 100.0 }, None);
+    layer.set_position(Point { x: 100.0, y: 100.0 }, None);
+    engine.update(0.016);
     // bounds rectangle is in layer coordinates, origin is 0,0
-    // assert!(!layer.bounds().contains(Point { x: 200.0, y: 200.0 }));
-    // assert!(layer.bounds().contains(Point { x: 50.0, y: 50.0 }));
+    assert!(!layer.cointains_point((200.0, 200.0)));
+    assert!(layer.cointains_point((150.0, 150.0)));
 }
 
 #[test]
@@ -26,38 +28,47 @@ pub fn scene_node_contains() {
     let engine = LayersEngine::new(1000.0, 1000.0);
     let layer = engine.new_layer();
     engine.scene_add_layer(layer.clone());
-
-    // const RECT_WIDTH: f32 = 100.0;
-    // const RECT_HEIGHT: f32 = 100.0;
-
+    layer.set_size(Size::points(100.0, 100.0), None);
     layer.set_anchor_point(Point { x: 0.5, y: 0.5 }, None);
-    // layer.set_size(
-    //     Point {
-    //         x: RECT_WIDTH as f32,
-    //         y: RECT_HEIGHT as f32,
+    layer.set_position(Point { x: 100.0, y: 100.0 }, None);
+
+    engine.update(0.016);
+
+    let point = Point { x: 150.0, y: 150.0 };
+
+    let node = engine.scene_layer_at(point);
+
+    assert!(layer.cointains_point(point));
+    assert!(node.is_some());
+
+    // FIXME we do not support rotation hit testing properly yet
+
+    // layer.set_rotation(
+    //     Point3d {
+    //         x: 0.0,
+    //         y: 0.0,
+    //         z: PI as f32 / 4.0,
     //     },
     //     None,
     // );
 
-    layer.set_position(Point { x: 160.0, y: 160.0 }, None);
+    // engine.update(0.016);
+    // let node = engine.scene_layer_at(point);
+    // assert!(!layer.cointains_point(point));
+    // assert!(node.is_none());
+}
 
-    engine.update(0.0);
+#[test]
+pub fn layer_contains_scale() {
+    let engine = LayersEngine::new(1000.0, 1000.0);
+    let layer = engine.new_layer();
+    layer.set_size(Size::points(100.0, 100.0), None);
 
-    let point = Point { x: 200.0, y: 200.0 };
+    engine.scene_add_layer(layer.clone());
 
-    let node = engine.scene_layer_at(point);
-
-    assert!(node.is_some());
-
-    layer.set_rotation(
-        Point3d {
-            x: 0.0,
-            y: 0.0,
-            z: PI as f32 / 4.0,
-        },
-        None,
-    );
-    engine.update(0.0);
-    let node = engine.scene_layer_at(point);
-    assert!(node.is_none());
+    layer.set_scale(Point { x: 2.0, y: 2.0 }, None);
+    engine.update(0.016);
+    // bounds rectangle is in layer coordinates, origin is 0,0
+    assert!(layer.cointains_point((199.0, 199.0)));
+    assert!(!layer.cointains_point((210.0, 210.0)));
 }
