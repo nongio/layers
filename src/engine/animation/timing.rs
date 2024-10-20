9025f544d86bbb3f6bdb6e43559dc6e421abb957
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2024-10-20 07:49:12 +0200

    transition helpers

--- src/engine/animation/timing.rs
+++ src/engine/animation/timing.rs
@@ -1,4 +1,4 @@
-use crate::easing::bezier_easing_function;
+// use crate::easing::bezier_easing_function;
 
 /// This module contains the animation related data structures
 #[repr(C)]
@@ -55,6 +55,103 @@ impl Easing {
             y2: 1.0,
         }
     }
+    pub fn ease_out_quad() -> Self {
+        Easing {
+            x1: 0.25,
+            y1: 0.46,
+            x2: 0.45,
+            y2: 0.94,
+        }
+    }
+    pub fn ease_in_quad() -> Self {
+        Easing {
+            x1: 0.55,
+            y1: 0.085,
+            x2: 0.68,
+            y2: 0.53,
+        }
+    }
+    pub fn ease_in_out_quad() -> Self {
+        Easing {
+            x1: 0.455,
+            y1: 0.03,
+            x2: 0.515,
+            y2: 0.955,
+        }
+    }
+    pub fn ease_out_cubic() -> Self {
+        Easing {
+            x1: 0.215,
+            y1: 0.61,
+            x2: 0.355,
+            y2: 1.0,
+        }
+    }
+    pub fn ease_in_cubic() -> Self {
+        Easing {
+            x1: 0.55,
+            y1: 0.055,
+            x2: 0.675,
+            y2: 0.19,
+        }
+    }
+    pub fn ease_in_out_cubic() -> Self {
+        Easing {
+            x1: 0.645,
+            y1: 0.045,
+            x2: 0.355,
+            y2: 1.0,
+        }
+    }
+    pub fn ease_out_quart() -> Self {
+        Easing {
+            x1: 0.165,
+            y1: 0.84,
+            x2: 0.44,
+            y2: 1.0,
+        }
+    }
+    pub fn ease_in_quart() -> Self {
+        Easing {
+            x1: 0.895,
+            y1: 0.03,
+            x2: 0.685,
+            y2: 0.22,
+        }
+    }
+    pub fn ease_in_out_quart() -> Self {
+        Easing {
+            x1: 0.77,
+            y1: 0.0,
+            x2: 0.175,
+            y2: 1.0,
+        }
+    }
+    pub fn ease_out_quint() -> Self {
+        Easing {
+            x1: 0.23,
+            y1: 1.0,
+            x2: 0.32,
+            y2: 1.0,
+        }
+    }
+    pub fn ease_in_quint() -> Self {
+        Easing {
+            x1: 0.755,
+            y1: 0.05,
+            x2: 0.855,
+            y2: 0.06,
+        }
+    }
+    pub fn ease_in_out_quint() -> Self {
+        Easing {
+            x1: 0.86,
+            y1: 0.0,
+            x2: 0.07,
+            y2: 1.0,
+        }
+    }
+
 }
 #[derive(Clone, Copy, Debug)]
 pub enum TimingFunction {
@@ -62,12 +159,53 @@ pub enum TimingFunction {
 }
 
 impl TimingFunction {
+    pub fn linear() -> Self {
+        TimingFunction::Easing(Easing::linear())
+    }
+    pub fn ease_in() -> Self {
+        TimingFunction::Easing(Easing::ease_in())
+    }
+    pub fn ease_out() -> Self {
+        TimingFunction::Easing(Easing::ease_out())
+    }
+    pub fn ease_in_out() -> Self {
+        TimingFunction::Easing(Easing::ease_in_out())
+    }
+    pub fn ease_out_quad() -> Self {
+        TimingFunction::Easing(Easing::ease_out_quad())
+    }
+    pub fn ease_in_quad() -> Self {
+        TimingFunction::Easing(Easing::ease_in_quad())
+    }
+    pub fn ease_in_out_quad() -> Self {
+        TimingFunction::Easing(Easing::ease_in_out_quad())
+    }
+    pub fn ease_out_cubic() -> Self {
+        TimingFunction::Easing(Easing::ease_out_cubic())
+    }
+    pub fn ease_in_cubic() -> Self {
+        TimingFunction::Easing(Easing::ease_in_cubic())
+    }
+    pub fn ease_in_out_cubic() -> Self {
+        TimingFunction::Easing(Easing::ease_in_out_cubic())
+    }
+    pub fn ease_out_quart() -> Self {
+        TimingFunction::Easing(Easing::ease_out_quart())
+    }
+    pub fn ease_in_quart() -> Self {
+        TimingFunction::Easing(Easing::ease_in_quart())
+    }
+    pub fn ease_in_out_quart() -> Self {
+        TimingFunction::Easing(Easing::ease_in_out_quart())
+    }
     pub fn value_at(&self, t: f32) -> f32 {
         let t = t.clamp(0.0, 1.0);
         match self {
             TimingFunction::Easing(Easing { x1, x2, y1, y2 }) => {
-                bezier_easing_function(*x1 as f64, *x2 as f64, *y1 as f64, *y2 as f64, t as f64)
-                    as f32
+                let ease = bezier_easing::bezier_easing(*x1, *y1, *x2, *y2).unwrap();
+                ease(t)
+                //bezier_easing_function(*x1 as f64, *x2 as f64, *y1 as f64, *y2 as f64, t as f64)
+                //    as f32
             }
         }
     }
