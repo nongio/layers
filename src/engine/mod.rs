2be23cf0d1ae85a7bf90b4e7f9258fc2a2453e40
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2024-10-27 10:20:01 +0100

    split engine / api

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -8,26 +8,26 @@
 //! - The *render* step uses the displaylist to generate a texture of the node
 //! - The *compose* step generates the final image using the textures
 
-pub(crate) mod draw_to_picture;
+mod layers_engine;
 mod stages;
 
 pub(crate) mod command;
+pub(crate) mod draw_to_picture;
 pub(crate) mod rendering;
-pub mod storage;
-
 pub(crate) mod scene;
 
 pub mod animation;
 pub mod node;
+pub mod storage;
+pub use layers_engine::LayersEngine;
 
-use crate::{
-    layers::layer::{model::PointerHandlerFunction, state::LayerDataProps, Layer, ModelLayer},
-    types::Point,
-};
 use core::fmt;
 use indextree::NodeId;
+use layers_engine::{initialize_engines, ENGINES, INIT};
 use node::ContainsPoint;
 
+#[cfg(feature = "debugger")]
+use layers_debug_server::DebugServerError;
 #[cfg(feature = "debugger")]
 #[allow(unused_imports)]
 use stages::send_debugger;
@@ -35,17 +35,11 @@ use stages::send_debugger;
 use stages::{nodes_for_layout, trigger_callbacks, update_node};
 use taffy::prelude::*;
 
-#[cfg(feature = "debugger")]
-use layers_debug_server::DebugServerError;
-
 use std::{
     collections::HashMap,
     hash::Hash,
     ops::Deref,
-    sync::{
-        atomic::{AtomicBool, AtomicUsize},
-        Arc, Once, RwLock,
-    },
+    sync::{atomic::AtomicUsize, Arc, RwLock},
 };
 
 use self::{
@@ -59,6 +53,11 @@ use self::{
     },
     storage::{FlatStorage, FlatStorageId, TreeStorageId, TreeStorageNode},
 };
+use crate::{
+    layers::layer::{model::PointerHandlerFunction, Layer},
+    types::Point,
+};
+
 #[derive(Clone)]
 pub struct Timestamp(f32);
 
@@ -88,10 +87,10 @@ pub struct AnimatedNodeChange {
 /// the progres can not be used to determine if the animation is finished
 /// because the animation could be reversed or looped
 #[derive(Clone, Debug)]
-struct AnimationState {
-    pub(crate) animation: Animation,
+pub struct AnimationState {
+    pub animation: Animation,
+    pub time: f32,
     pub(crate) progress: f32,
-    pub(crate) time: f32,
     pub(crate) is_started: bool,
     pub(crate) is_running: bool,
     pub(crate) is_finished: bool,
@@ -209,15 +208,6 @@ pub enum PointerEventType {
     Up,
 }
 
-static INIT: Once = Once::new();
-static ENGINE_ID: AtomicUsize = AtomicUsize::new(0);
-static mut ENGINES: Option<RwLock<HashMap<usize, Arc<Engine>>>> = None;
-
-fn initialize_engines() {
-    unsafe {
-        ENGINES = Some(RwLock::new(HashMap::new()));
-    }
-}
 pub(crate) struct Engine {
     pub id: usize,
     pub(crate) scene: Arc<Scene>,
@@ -234,9 +224,10 @@ pub(crate) struct Engine {
     current_hover_node: RwLock<Option<NodeRef>>,
     pointer_handlers: FlatStorage<PointerCallback>,
 }
+
 #[derive(Clone, Copy, Debug)]
 pub struct TransactionRef {
-    pub id: FlatStorageId,
+    pub value_id: FlatStorageId,
     pub(crate) engine_id: usize,
 }
 
@@ -292,216 +283,6 @@ impl From<NodeRef> for TreeStorageId {
 impl From<NodeRef> for usize {
     fn from(node_ref: NodeRef) -> Self {
         node_ref.0.into()
-    }
-}
-/// Main struct to interact with the engine
-/// ## Usage: Setup a basic scene with a root layer
-/// ```rust
-/// use layers::prelude::*;
-///
-/// let engine = LayersEngine::new(800.0, 600.0);
-/// let layer = engine.new_layer();
-/// let engine = LayersEngine::new(1024.0, 768.0);
-/// let root_layer = engine.new_layer();
-/// root_layer.set_position(Point { x: 0.0, y: 0.0 }, None);
-
-/// root_layer.set_background_color(
-///     PaintColor::Solid {
-///         color: Color::new_rgba255(180, 180, 180, 255),
-///     },
-///    None,
-/// );
-/// root_layer.set_border_corner_radius(10.0, None);
-/// root_layer.set_layout_style(taffy::Style {
-///     position: taffy::Position::Absolute,
-///     display: taffy::Display::Flex,
-///     flex_direction: taffy::FlexDirection::Column,
-///     justify_content: Some(taffy::JustifyContent::Center),
-///     align_items: Some(taffy::AlignItems::Center),
-///     ..Default::default()
-/// });
-/// engine.scene_add_layer(root_layer.clone());
-/// ```
-/// ## Usage: Update the engine
-/// ```rust
-/// use layers::prelude::*;
-///
-/// let engine = LayersEngine::new(800.0, 600.0);
-/// // setup the scene...
-/// engine.update(0.016);
-/// ```
-#[derive(Clone)]
-pub struct LayersEngine {
-    pub(crate) engine: Arc<Engine>,
-}
-
-impl LayersEngine {
-    pub fn new(width: f32, height: f32) -> Self {
-        let engines = unsafe {
-            INIT.call_once(initialize_engines);
-            ENGINES.as_ref().unwrap()
-        };
-        let id = ENGINE_ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-        let new_engine = Engine::create(id, width, height);
-        engines.write().unwrap().insert(id, new_engine.clone());
-        Self { engine: new_engine }
-    }
-    #[cfg(feature = "debugger")]
-    pub fn start_debugger(&self) {
-        layers_debug_server::start_debugger_server(self.engine.clone());
-    }
-
-    pub fn set_scene_size(&self, width: f32, height: f32) {
-        self.engine.scene.set_size(width, height);
-    }
-    pub fn new_layer(&self) -> Layer {
-        let model = Arc::new(ModelLayer::default());
-
-        let mut lt = self.engine.layout_tree.write().unwrap();
-
-        let layout = lt.new_leaf(Style::default()).unwrap();
-
-        Layer {
-            engine: self.engine.clone(),
-            model,
-            id: Arc::new(RwLock::new(None)),
-            key: Arc::new(RwLock::new(String::new())),
-            layout_node_id: layout,
-            hidden: Arc::new(AtomicBool::new(false)),
-            pointer_events: Arc::new(AtomicBool::new(true)),
-            image_cache: Arc::new(AtomicBool::new(false)),
-            state: Arc::new(RwLock::new(LayerDataProps::default())),
-            effect: Arc::new(RwLock::new(None)),
-        }
-    }
-    pub fn new_animation(&self, transition: Transition, autostart: bool) -> AnimationRef {
-        self.engine
-            .add_animation_from_transition(transition, autostart)
-    }
-    pub fn attach_animation(&self, transaction: TransactionRef, animation: AnimationRef) {
-        self.engine.attach_animation(transaction, animation);
-    }
-    pub fn start_animation(&self, animation: AnimationRef, delay: f32) {
-        self.engine.start_animation(animation, delay);
-    }
-    pub fn add_animated_changes(
-        &self,
-        animated_changes: &[AnimatedNodeChange],
-        animation: impl Into<Option<AnimationRef>>,
-    ) -> Vec<TransactionRef> {
-        self.engine.schedule_changes(animated_changes, animation)
-    }
-    pub fn update(&self, dt: f32) -> bool {
-        self.engine.update(dt)
-    }
-    pub fn update_nodes(&self) -> skia_safe::Rect {
-        self.engine.update_nodes()
-    }
-    pub fn scene_add_layer(&self, layer: impl Into<Layer>) -> NodeRef {
-        self.engine.scene_add_layer(layer, None)
-    }
-    pub fn scene_add_layer_to(
-        &self,
-        layer: impl Into<Layer>,
-        parent: impl Into<Option<NodeRef>>,
-    ) -> NodeRef {
-        let parent = parent.into();
-        self.engine.scene_add_layer(layer, parent)
-    }
-
-    pub fn scene_add_layer_to_positioned(
-        &self,
-        layer: impl Into<Layer>,
-        parent: impl Into<Option<NodeRef>>,
-    ) -> NodeRef {
-        let parent = parent.into();
-        self.engine.scene_add_layer_to_positioned(layer, parent)
-    }
-    pub fn scene_remove_layer(&self, node: impl Into<Option<NodeRef>>) {
-        if let Some(node) = node.into() {
-            self.engine.mark_for_delete(node);
-        }
-    }
-
-    pub fn scene_set_root(&self, layer: impl Into<Layer>) -> NodeRef {
-        self.engine.scene_set_root(layer)
-    }
-    pub fn scene_get_node(&self, node: &NodeRef) -> Option<TreeStorageNode<SceneNode>> {
-        self.engine.scene_get_node(node)
-    }
-    pub fn scene_get_node_parent(&self, node: &NodeRef) -> Option<NodeRef> {
-        self.engine.scene_get_node_parent(node)
-    }
-    pub fn scene(&self) -> &Arc<Scene> {
-        &self.engine.scene
-    }
-    pub fn scene_root(&self) -> Option<NodeRef> {
-        *self.engine.scene_root.read().unwrap()
-    }
-    pub fn step_time(&self, dt: f32) {
-        self.engine.step_time(dt)
-    }
-
-    pub fn scene_layer_at(&self, point: Point) -> Option<NodeRef> {
-        self.engine.layer_at(point)
-    }
-    pub fn damage(&self) -> skia_safe::Rect {
-        *self.engine.damage.read().unwrap()
-    }
-    #[profiling::function]
-    pub fn clear_damage(&self) {
-        *self.engine.damage.write().unwrap() = skia_safe::Rect::default();
-    }
-    pub fn root_layer(&self) -> Option<SceneNode> {
-        let root_id = self.scene_root()?;
-        let node = self.scene_get_node(&root_id)?;
-        Some(node.get().clone())
-    }
-    pub fn pointer_move(&self, point: impl Into<Point>, root_id: impl Into<Option<NodeId>>) {
-        self.engine.pointer_move(point, root_id);
-    }
-    pub fn pointer_button_down(&self) {
-        self.engine.pointer_button_down();
-    }
-    pub fn pointer_button_up(&self) {
-        self.engine.pointer_button_up();
-    }
-    pub fn current_hover(&self) -> Option<NodeRef> {
-        self.engine.current_hover()
-    }
-
-    pub fn on_finish<F: Into<TransactionCallback>>(
-        &self,
-        transaction: TransactionRef,
-        handler: F,
-        once: bool,
-    ) {
-        self.engine.on_finish(transaction, handler, once);
-    }
-    pub fn on_update<F: Into<TransactionCallback>>(
-        &self,
-        transaction: TransactionRef,
-        handler: F,
-        once: bool,
-    ) {
-        self.engine.on_update(transaction, handler, once);
-    }
-    pub fn on_start<F: Into<TransactionCallback>>(
-        &self,
-        transaction: TransactionRef,
-        handler: F,
-        once: bool,
-    ) {
-        self.engine.on_start(transaction, handler, once);
-    }
-    pub fn get_pointer_position(&self) -> Point {
-        self.engine.get_pointer_position()
-    }
-}
-
-impl std::fmt::Debug for LayersEngine {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("LayersEngine").finish()
     }
 }
 
@@ -721,7 +502,7 @@ impl Engine {
         self.add_animation(
             Animation {
                 start,
-                duration: transition.duration,
+                // duration: transition.duration,
                 timing: transition.timing,
             },
             autostart,
@@ -755,7 +536,7 @@ impl Engine {
         animation: impl Into<Option<AnimationRef>>,
     ) -> Vec<TransactionRef> {
         let animation = animation.into();
-        let mut transactions = Vec::with_capacity(animated_changes.len());
+        let mut inserted_transactions = Vec::with_capacity(animated_changes.len());
         for animated_node_change in animated_changes {
             let mut animated_node_change = animated_node_change.clone();
             if animation.is_some() {
@@ -763,14 +544,20 @@ impl Engine {
             }
             let transaction_id = animated_node_change.change.value_id();
             let transaction = TransactionRef {
-                id: self
+                value_id: self
                     .transactions
                     .insert_with_id(animated_node_change.clone(), transaction_id),
                 engine_id: self.id,
             };
-            transactions.push(transaction);
+            inserted_transactions.push(transaction);
         }
-        transactions
+        inserted_transactions
+    }
+    pub fn get_transaction_for_value(&self, value_id: usize) -> Option<AnimatedNodeChange> {
+        self.transactions.with_data(|d| d.get(&value_id).cloned())
+    }
+    pub fn get_animation(&self, animation: AnimationRef) -> Option<AnimationState> {
+        self.animations.with_data(|d| d.get(&animation.0).cloned())
     }
     pub fn schedule_change(
         &self,
@@ -787,14 +574,14 @@ impl Engine {
                 node_id: target_id,
             };
             TransactionRef {
-                id: self
+                value_id: self
                     .transactions
                     .insert_with_id(animated_node_change, transaction_id),
                 engine_id: self.id,
             }
         } else {
             TransactionRef {
-                id: FlatStorageId::default(),
+                value_id: FlatStorageId::default(),
                 engine_id: self.id,
             }
         }
@@ -803,9 +590,16 @@ impl Engine {
     pub fn attach_animation(&self, transaction: TransactionRef, animation: AnimationRef) {
         let transactions = self.transactions.data();
         let mut transactions = transactions.write().unwrap();
-        if let Some(transaction) = transactions.get_mut(&transaction.id) {
+        if let Some(transaction) = transactions.get_mut(&transaction.value_id) {
             transaction.animation_id = Some(animation);
+            // FIXME should cancel existing animation?
         }
+    }
+    pub fn cancel_animation(&self, animation: AnimationRef) {
+        self.animations.with_data_mut(|d| {
+            println!("cancel animation {:?}", animation.0);
+            d.remove(&animation.0);
+        });
     }
     pub fn step_time(&self, dt: f32) {
         let mut timestamp = self.timestamp.write().unwrap();
@@ -928,7 +722,7 @@ impl Engine {
     ) {
         let mut ch = self
             .transaction_handlers
-            .get(&transaction.id)
+            .get(&transaction.value_id)
             .unwrap_or_else(TransitionCallbacks::new);
 
         match event_type {
@@ -937,7 +731,8 @@ impl Engine {
             TransactionEventType::Update => ch.on_update.push(handler),
         };
 
-        self.transaction_handlers.insert_with_id(ch, transaction.id);
+        self.transaction_handlers
+            .insert_with_id(ch, transaction.value_id);
     }
 
     pub fn on_start<F: Into<TransactionCallback>>(
