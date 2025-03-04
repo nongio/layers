3662c54937f61a4b740d364190127fcd3732b434
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2025-03-04 20:56:26 +0100

    bugfix! renderlayer opacity is not pre-multiplied

--- tests/opacity.rs
+++ tests/opacity.rs
@@ -29,22 +29,4 @@ mod tests {
 
         assert_eq!(render_layer.opacity, 1.0);
     }
-    #[test]
-    pub fn layer_parent_opacity() {
-        let engine = Engine::create(1000.0, 1000.0);
-        let wrap = engine.new_layer();
-
-        wrap.set_opacity(0.0, None);
-
-        engine.add_layer(&wrap);
-
-        let layer = engine.new_layer();
-
-        engine.append_layer(&layer, wrap.id);
-
-        engine.update(0.016);
-        let render_layer = engine.render_layer(&layer).unwrap();
-
-        assert_eq!(render_layer.opacity, 0.0);
-    }
 }
