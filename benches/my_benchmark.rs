a4e51be57792311017c6b7ccd6fad6f8f9e962f7
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2025-03-05 21:23:23 +0100

    add tokio runtime to benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -7,6 +7,8 @@ use lay_rs::{engine::LayersEngine, prelude::*, types::*};
 
 #[allow(dead_code)]
 fn criterion_benchmark_update(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
     let mut group = c.benchmark_group("engine::update");
     group.measurement_time(Duration::from_secs(10));
     // Define different numbers of children to test
@@ -14,29 +16,33 @@ fn criterion_benchmark_update(c: &mut Criterion) {
 
     for &count in &child_counts {
         group.bench_with_input(BenchmarkId::from_parameter(count), &count, |b, &count| {
-            let engine = LayersEngine::new(2048.0, 2048.0);
-            let root = engine.new_layer();
-            engine.scene_set_root(root);
-            let mut layers = Vec::<Layer>::new();
-            for _ in 0..count {
-                let layer = engine.new_layer();
-                engine.add_layer(layer.clone());
-                layers.push(layer);
-            }
-            for layer in layers.iter() {
-                layer.set_size(
-                    Size::points(1000.0, 1000.0),
-                    Some(Transition::ease_out_quad(10000.0)),
-                );
-                layer.set_opacity(0.5, Some(Transition::ease_in_quad(10000.0)));
-            }
-            b.iter(|| engine.update(black_box(0.016)));
+            rt.block_on(async {
+                let engine = LayersEngine::new(2048.0, 2048.0);
+                let root = engine.new_layer();
+                engine.scene_set_root(root);
+                let mut layers = Vec::<Layer>::new();
+                for _ in 0..count {
+                    let layer = engine.new_layer();
+                    engine.add_layer(layer.clone());
+                    layers.push(layer);
+                }
+                for layer in layers.iter() {
+                    layer.set_size(
+                        Size::points(1000.0, 1000.0),
+                        Some(Transition::ease_out_quad(10000.0)),
+                    );
+                    layer.set_opacity(0.5, Some(Transition::ease_in_quad(10000.0)));
+                }
+                b.iter(|| engine.update(black_box(0.016)));
+            });
         });
     }
 }
 
 #[allow(dead_code)]
 fn criterion_benchmark_append(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
     let mut group = c.benchmark_group("engine::append");
     group.measurement_time(Duration::from_secs(10));
     // Define different numbers of children to test
@@ -44,16 +50,18 @@ fn criterion_benchmark_append(c: &mut Criterion) {
 
     for &count in &child_counts {
         group.bench_with_input(BenchmarkId::from_parameter(count), &count, |b, &_count| {
-            let engine = LayersEngine::new(2048.0, 2048.0);
-            let root = engine.new_layer();
-            engine.scene_set_root(root);
+            rt.block_on(async {
+                let engine = LayersEngine::new(2048.0, 2048.0);
+                let root = engine.new_layer();
+                engine.scene_set_root(root);
 
-            engine.update(black_box(0.016));
-
-            b.iter(|| {
-                let layer = engine.new_layer();
-                engine.add_layer(layer.clone());
                 engine.update(black_box(0.016));
+
+                b.iter(|| {
+                    let layer = engine.new_layer();
+                    engine.add_layer(layer.clone());
+                    engine.update(black_box(0.016));
+                });
             });
         });
     }
@@ -61,6 +69,7 @@ fn criterion_benchmark_append(c: &mut Criterion) {
 
 #[allow(dead_code)]
 fn criterion_benchmark_remove(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
     let mut group = c.benchmark_group("engine::remove");
     group.measurement_time(Duration::from_secs(10));
     // Define different numbers of children to test
@@ -68,21 +77,23 @@ fn criterion_benchmark_remove(c: &mut Criterion) {
 
     for &count in &child_counts {
         group.bench_with_input(BenchmarkId::from_parameter(count), &count, |b, &count| {
-            let engine = LayersEngine::new(2048.0, 2048.0);
-            let root = engine.new_layer();
-            engine.scene_set_root(root);
-            let mut layers = Vec::<Layer>::new();
-            for _ in 0..count {
-                let layer = engine.new_layer();
-                engine.add_layer(layer.clone());
-                layers.push(layer);
-            }
-            engine.update(black_box(0.016));
-            b.iter(|| {
-                if let Some(layer) = layers.pop() {
-                    layer.remove();
-                    engine.update(black_box(0.016));
+            rt.block_on(async {
+                let engine = LayersEngine::new(2048.0, 2048.0);
+                let root = engine.new_layer();
+                engine.scene_set_root(root);
+                let mut layers = Vec::<Layer>::new();
+                for _ in 0..count {
+                    let layer = engine.new_layer();
+                    engine.add_layer(layer.clone());
+                    layers.push(layer);
                 }
+                engine.update(black_box(0.016));
+                b.iter(|| {
+                    if let Some(layer) = layers.pop() {
+                        layer.remove();
+                        engine.update(black_box(0.016));
+                    }
+                });
             });
         });
     }
@@ -90,6 +101,7 @@ fn criterion_benchmark_remove(c: &mut Criterion) {
 
 #[allow(dead_code)]
 fn criterion_benchmark_pointer_move(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
     let mut group = c.benchmark_group("engine::pointer_move");
     group.measurement_time(Duration::from_secs(10));
     // Define different numbers of children to test
@@ -97,34 +109,36 @@ fn criterion_benchmark_pointer_move(c: &mut Criterion) {
 
     for &count in &child_counts {
         group.bench_with_input(BenchmarkId::from_parameter(count), &count, |b, &count| {
-            let engine = LayersEngine::new(2048.0, 2048.0);
-            let root = engine.new_layer();
-            engine.scene_set_root(root);
-            let mut layers = Vec::<Layer>::new();
-            for _ in 0..count {
-                let layer = engine.new_layer();
-                layer.set_layout_style(Style {
-                    position: taffy::Position::Absolute,
-                    ..Default::default()
-                });
-                layer.set_size(Size::points(100.0, 100.0), None);
-                layer.set_position(
-                    (
-                        rand::random::<f32>() * 2048.0,
-                        rand::random::<f32>() * 2048.0,
-                    ),
-                    None,
-                );
-                engine.add_layer(layer.clone());
-                layers.push(layer);
-            }
-            engine.update(black_box(0.016));
+            rt.block_on(async {
+                let engine = LayersEngine::new(2048.0, 2048.0);
+                let root = engine.new_layer();
+                engine.scene_set_root(root);
+                let mut layers = Vec::<Layer>::new();
+                for _ in 0..count {
+                    let layer = engine.new_layer();
+                    layer.set_layout_style(Style {
+                        position: taffy::Position::Absolute,
+                        ..Default::default()
+                    });
+                    layer.set_size(Size::points(100.0, 100.0), None);
+                    layer.set_position(
+                        (
+                            rand::random::<f32>() * 2048.0,
+                            rand::random::<f32>() * 2048.0,
+                        ),
+                        None,
+                    );
+                    engine.add_layer(layer.clone());
+                    layers.push(layer);
+                }
+                engine.update(black_box(0.016));
 
-            b.iter(|| {
-                let x = rand::random::<f32>() * 2048.0;
-                let y = rand::random::<f32>() * 2048.0;
-                let p: Point = (x, y).into();
-                engine.pointer_move(p, None);
+                b.iter(|| {
+                    let x = rand::random::<f32>() * 2048.0;
+                    let y = rand::random::<f32>() * 2048.0;
+                    let p: Point = (x, y).into();
+                    engine.pointer_move(p, None);
+                });
             });
         });
     }
